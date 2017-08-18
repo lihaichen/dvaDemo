e3d9b8d9872d3f91819a2a07d50da3022f3755af
lihaichen <li8303@yeah.net>
2017-08-18 14:37:07 +0800

    添加header头 加入antd的样式处理

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,9 @@
 import React, {Component, PropTypes} from 'react';
+import { Menu } from 'antd';
 import './index.less';
 const prefixCls = 'Header';
+
+const SubMenu = Menu.SubMenu;
 
 export default class Header extends Component {
   static propTypes = {};
@@ -15,7 +18,25 @@ export default class Header extends Component {
   render() {
     return (
       <div className={`${prefixCls}`}>
-        {this.props.children}
+        <div className={`${prefixCls}-logo`}/>
+        <Menu
+          mode="horizontal"
+          theme="dark"
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="task">
+            任务库
+          </Menu.Item>
+          <Menu.Item key="12323">
+            技能法
+          </Menu.Item>
+          <Menu.Item key="tools">
+            工具库
+          </Menu.Item>
+          <Menu.Item key="system">
+            系统管理
+          </Menu.Item>
+        </Menu>
       </div>);
   }
 }
